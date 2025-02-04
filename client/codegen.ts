8f3be8950ac36c82ce9a4359823530ac75344064
Thomas <thomas.rc.fr@gmail.com>
2025-02-04 17:04:35 +0100

    init project

---
+++ client/codegen.ts
@@ -0,0 +1,17 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+ 
+const config: CodegenConfig = {
+  schema: 'https://rickandmortyapi.com/graphql',
+  documents: ['src/**/*.tsx'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    './src/gql/': {
+      preset: 'client',
+      config: {
+        avoidOptionals: true
+      },
+    }
+  }
+}
+ 
+export default config
